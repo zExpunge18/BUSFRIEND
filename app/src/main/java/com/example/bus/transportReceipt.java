144387ea1dd65ef1465d99df4cb738be97095697
zExpunge18 <vbmarquez18@gmail.com>
2019-11-14 11:22:44 +0800

    Reciept Shared Preference has been initialized

--- app/src/main/java/com/example/bus/transportReceipt.java
+++ app/src/main/java/com/example/bus/transportReceipt.java
@@ -1,6 +1,7 @@
 package com.example.bus;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,11 +10,25 @@ import android.widget.Button;
 public class transportReceipt extends AppCompatActivity {
 
     Button btnHome;
+    int id, payment, booked_seat, price;
+    String fullname, destinationFrom, destinationTo, date, bus_name, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport_receipt);
+        SharedPreferences sp = getApplication().getSharedPreferences("trip_details", MODE_PRIVATE);
+        id = sp.getInt("id",0);
+        payment = sp.getInt("payment",0);
+        booked_seat = sp.getInt("booked_seat",0);
+        price = sp.getInt("price",0);
+        fullname = sp.getString("fullname", null);
+        destinationFrom = sp.getString("destinationFrom", null);
+        destinationTo = sp.getString("destinationTo", null);
+        date = sp.getString("date", null);
+        bus_name = sp.getString("bus_name", null);
+        email = sp.getString("email", null);
+
         btnHome = findViewById(R.id.btnRrturnHome);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
